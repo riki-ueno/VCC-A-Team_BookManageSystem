b5e011140a0a6930c66ea6ea9b5e421283f505f9
riki.ueno <ri-ueno@virtualex.co.jp>
2020-05-19 16:42:38 +0900

    書籍レンタル機能を追加

--- src/app/dao/RentalsDAO.java
+++ src/app/dao/RentalsDAO.java
@@ -1,7 +1,30 @@
 package app.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import app.model.Rental;
+
 public class RentalsDAO extends DAOBase{
-	RentalsDAO() {
+	public RentalsDAO() {
 		super();
+	}
+
+	public boolean create(Rental rental) {
+		String sql =
+				"INSERT INTO RENTALS (ACCOUNT_ID, BOOK_ID) " +
+				"VALUES (?, ?)";
+		try (
+				PreparedStatement pstmt = createPreparedStatement(sql);
+		) {
+
+			pstmt.setInt(1, rental.getAccountId());
+			pstmt.setInt(2, rental.getBookId());
+			int count = pstmt.executeUpdate();
+
+			return count == 1;
+		} catch (SQLException e) {
+			throw new RuntimeException(String.format("検索処理の実施中にエラーが発生しました。詳細:[%s]", e.getMessage()));
+		}
 	}
 }
--- src/app/servlet/book/RentalServlet.java
+++ src/app/servlet/book/RentalServlet.java
@@ -1,12 +1,17 @@
 package app.servlet.book;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import app.service.RentalService;
 
 /**
  * Servlet implementation class RentalServlet
@@ -23,7 +28,11 @@ public class RentalServlet extends HttpServlet {
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int accountId = (int) request.getSession().getAttribute("account_id");
+		int bookId = Integer.parseInt(request.getParameter("book[id]"));
 
+		boolean result = RentalService.call(bookId, accountId);
+		PrintWriter pw = response.getWriter();
+		pw.append(new ObjectMapper().writeValueAsString(result));
 	}
-
 }
