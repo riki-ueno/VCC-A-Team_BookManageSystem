4976770f06706abbec327ef8010bc60b5d7d1609
ma_naruse <ma-naruse@virtualex.co.jp>
2020-05-20 13:51:50 +0900

    add method to convert genreName to genreId

--- src/app/dao/GenresDAO.java
+++ src/app/dao/GenresDAO.java
@@ -8,16 +8,14 @@ import java.util.List;
 
 import app.model.Genre;
 
-public class GenresDAO extends DAOBase{
+public class GenresDAO extends DAOBase {
 	public GenresDAO() {
 		super();
 	}
 
 	public List<Genre> all() {
 		String sql = "SELECT name FROM genres order by name";
-		try (
-				PreparedStatement pstmt = createPreparedStatement(sql);
-		) {
+		try (PreparedStatement pstmt = createPreparedStatement(sql);) {
 			ResultSet rs = pstmt.executeQuery();
 			List<Genre> genreList = new ArrayList<>();
 
@@ -28,6 +26,54 @@ public class GenresDAO extends DAOBase{
 			}
 
 			return genreList;
+
+		} catch (SQLException e) {
+			throw new RuntimeException(String.format("検索処理の実施中にエラーが発生しました。詳細:[%s]", e.getMessage()));
+		}
+	}
+
+	public boolean create(Genre genre) {
+		String sql = "INSERT INTO genres (id, name) " + "VALUES (?, ?)";
+		try (PreparedStatement pstmt = createPreparedStatement(sql);) {
+
+			pstmt.setInt(1, genre.getId());
+			pstmt.setString(2, genre.getName());
+			int count = pstmt.executeUpdate();
+
+			return count == 1;
+		} catch (SQLException e) {
+			throw new RuntimeException(String.format("検索処理の実施中にエラーが発生しました。詳細:[%s]", e.getMessage()));
+		}
+	}
+
+	public int[] convertGenreNameArrayToGenreIdArray(String[] genreNames) {
+		int[] genreIds = new int[genreNames.length];
+		for (int i = 0; i < genreNames.length; i++) {
+			String genreName = genreNames[i];
+			int genreId = convertGenreNameToGenreId(genreName);
+			genreIds[i] = genreId;
+		}
+		return genreIds;
+	}
+
+	public int convertGenreNameToGenreId(String genreName) {
+		String sql = "SELECT g.id FROM genres g WHERE g.name = '" + genreName + "'";
+		try (PreparedStatement pstmt = createPreparedStatement(sql);) {
+			ResultSet rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				int genreId = rs.getInt(1);
+				return genreId;
+			} else {
+				GenresDAO genreDao = new GenresDAO();
+				List<Genre> genreList = genreDao.all();
+				int newGenreId = genreList.size() + 1;
+				Genre genre = new Genre();
+				genre.setId(newGenreId);
+				genre.setName(genreName);
+				genreDao.create(genre);
+				return newGenreId;
+			}
 
 		} catch (SQLException e) {
 			throw new RuntimeException(String.format("検索処理の実施中にエラーが発生しました。詳細:[%s]", e.getMessage()));
