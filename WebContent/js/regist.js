afb76ea46939b9daad35e6e0ce06601d9bdcf7a3
ma_naruse <ma-naruse@virtualex.co.jp>
2020-05-21 12:41:45 +0900

    add regist continue method

--- WebContent/js/regist.js
+++ WebContent/js/regist.js
@@ -2,30 +2,30 @@
  * 
  */
 
-var LoginCertificate = function(){
+var LoginCertificate = function() {
 	$.ajax({
 		type : 'GET',
-		dataType:'json',
+		dataType : 'json',
 		url : '/BookManageSystem/api/auth/loginCertification',
 		success : function(json) {
-			if(json.result === "true"){
-			}else{
+			if (json.result === "true") {
+			} else {
 				alert('ログインしてください。');
 				location.href = "./login.html"
 			}
 		},
-		error:function(XMLHttpRequest, textStatus, errorThrown){
+		error : function(XMLHttpRequest, textStatus, errorThrown) {
 			alert('データベースへの更新に失敗しました。');
 			console.log(errorThrown)
 		}
 	});
 }
 
-
 var authorNameFormCounter = 1;
 var authorNamesStr;
 var genreNameFormCounter = 1;
 var genreNamesStr;
+var publisherNamesStr;
 
 function setAuthorNamesToDatalist() {
 	$.ajax({
@@ -40,7 +40,7 @@ function setAuthorNamesToDatalist() {
 			$('#author_names_1').append(authorNamesStr);
 		},
 		error : function() {
-			alert('データの通信に失敗しました。');
+			alert('筆者一覧の取得に失敗しました。');
 		}
 	});
 }
@@ -58,73 +58,58 @@ function setGenreNamesToDatalist() {
 			$('#genre_names_1').append(genreNamesStr);
 		},
 		error : function() {
-			alert('データの通信に失敗しました。');
+			alert('ジャンル一覧の取得に失敗しました。');
 		}
 	});
 }
+
+function setPublisherNamesToDataList() {
+	$.ajax({
+		type : 'GET',
+		url : '/BookManageSystem/api/publisher/index',
+		dataType : 'json',
+		success : function(publisherDatas) {
+			for (var i = 0; i < publisherDatas.length; i++) {
+				var publisherData = publisherDatas[i];
+				publisherNamesStr += '<option>' + publisherData.name + '</option>';
+			}
+			$('#publisher_names').append(publisherNamesStr);
+		},
+		error : function() {
+			alert('出版社一覧の取得に失敗しました。');
+		}
+	});
+}
+
 function addAuthorNameForm() {
 	authorNameFormCounter++;
 	$('#author_name_forms').append(
-			'<br><input type="text" class="author_name" id="author_name_' + authorNameFormCounter + '" list="author_names_' + authorNameFormCounter + '" >'
-					+ '<datalist id="author_names_' + authorNameFormCounter + '"></datalist>');
+			'<br><input type="text" class="author_name" id="author_name_' + authorNameFormCounter + '" list="author_names_' + authorNameFormCounter
+					+ '" required >' + '<datalist id="author_names_' + authorNameFormCounter + '"></datalist>');
 	$('#author_names_' + authorNameFormCounter).append(authorNamesStr);
 }
 
 function addGenreNameForm() {
 	genreNameFormCounter++;
 	$('#genre_name_forms').append(
-			'<br><input type="text" class="genre_name" id="genre_name_' + genreNameFormCounter + '" list="genre_names_' + genreNameFormCounter + '" >'
-					+ '<datalist id="genre_names_' + genreNameFormCounter + '"></datalist>');
+			'<br><input type="text" class="genre_name" id="genre_name_' + genreNameFormCounter + '" list="genre_names_' + genreNameFormCounter
+					+ '" required >' + '<datalist id="genre_names_' + genreNameFormCounter + '"></datalist>');
 	$('#genre_names_' + genreNameFormCounter).append(genreNamesStr);
 }
-
-
-function　setTestValues() {
-	$('#book_title').val('題名');
-	$('#author_name_1').val('筆者名1');
-	addAuthorNameForm();
-	$('#author_name_2').val('筆者名2');
-	$('#publisher_name').val('出版社名');
-	$('#genre_name_1').val('ジャンル1');
-	addGenreNameForm();
-	$('#genre_name_2').val('ジャンル2');
-	addGenreNameForm();
-	$('#genre_name_3').val('ジャンル3');
-	$('#purchaser_name').val('購入者');
-	$('#purchased_at').val('2020-05-01');
-}
-
-function test(){
-	var date = $('#purchased_at').val();
-	console.log(date);
-}
-
-
-$(document).ready(function() {
-	'use strict';
-	LoginCertificate();
-	setAuthorNamesToDatalist();
-	setGenreNamesToDatalist();
-	$('#set_test_value_button').click(setTestValues);
-	$('#add_author_name_form').click(addAuthorNameForm);
-	$('#add_genre_name_form').click(addGenreNameForm);
-	$('#regist').click(regist);
-	$('#date_value_test_button').click(test);
-});
 
 function regist() {
 	var title = $('#book_title').val();
 	var publisherName = $('#publisher_name').val();
 	var purchaserName = $('#purchaser_name').val();
 	var purchasedAt = $('#purchased_at').val();
-	var authorNames = [] ;
+	var authorNames = [];
 	var authorNameElements = $('.author_name');
-	for(var i=0;i<authorNameElements.length;i++){
+	for (var i = 0; i < authorNameElements.length; i++) {
 		authorNames.push(authorNameElements[i].value);
 	}
-	var genreNames = [] ;
+	var genreNames = [];
 	var genreNameElements = $('.genre_name');
-	for(var i=0;i<genreNameElements.length;i++){
+	for (var i = 0; i < genreNameElements.length; i++) {
 		genreNames.push(genreNameElements[i].value);
 	}
 	var requestQuery = {
@@ -134,24 +119,79 @@ function regist() {
 			purchasedAt : purchasedAt
 		},
 		genres : genreNames,
-		authors :authorNames,
+		authors : authorNames,
 		publisher : {
 			name : publisherName
 		}
 	};
 	console.log(requestQuery);
 	$.ajax({
-	 type : 'POST',
-	 url : '/BookManageSystem/api/book/regist',
-	 dataType:'json',
-	 data : requestQuery,
-	 success : function() {
-		 alert('書籍の登録が完了しました。');
-	 },
-	 error : function() {
-		 alert('登録できませんでした。入力情報を確認してください。');
-	 }
-	 });
+		type : 'POST',
+		url : '/BookManageSystem/api/book/regist',
+		dataType : 'text',
+		data : requestQuery,
+		success : function(bookId) {
+			alert('書籍の登録が完了しました。');
+			console.log(bookId);
+			location.href = './bookDetail.html/?bookId=' + bookId;
+		},
+		error : function() {
+			alert('登録できませんでした。入力情報を確認してください。');
+		}
+	});
 }
 
+function registContinue() {
+	var title = $('#book_title').val();
+	var publisherName = $('#publisher_name').val();
+	var purchaserName = $('#purchaser_name').val();
+	var purchasedAt = $('#purchased_at').val();
+	var authorNames = [];
+	var authorNameElements = $('.author_name');
+	for (var i = 0; i < authorNameElements.length; i++) {
+		authorNames.push(authorNameElements[i].value);
+	}
+	var genreNames = [];
+	var genreNameElements = $('.genre_name');
+	for (var i = 0; i < genreNameElements.length; i++) {
+		genreNames.push(genreNameElements[i].value);
+	}
+	var requestQuery = {
+		book : {
+			title : title,
+			purchaserName : purchaserName,
+			purchasedAt : purchasedAt
+		},
+		genres : genreNames,
+		authors : authorNames,
+		publisher : {
+			name : publisherName
+		}
+	};
+	console.log(requestQuery);
+	$.ajax({
+		type : 'POST',
+		url : '/BookManageSystem/api/book/regist',
+		dataType : 'text',
+		data : requestQuery,
+		success : function(bookId) {
+			alert('書籍の登録が完了しました。');
+			location.reload();
+		},
+		error : function() {
+			alert('登録できませんでした。入力情報を確認してください。');
+		}
+	});
+}
 
+$(document).ready(function() {
+	'use strict';
+	LoginCertificate();
+	setAuthorNamesToDatalist();
+	setGenreNamesToDatalist();
+	setPublisherNamesToDataList();
+	$('#add_author_name_form').click(addAuthorNameForm);
+	$('#add_genre_name_form').click(addGenreNameForm);
+	$('#regist').click(regist);
+	$('#regist_continue').click(registContinue);
+});
