0f37958e5c6d657571089df5076ef304b77fb5a0
riki.ueno <ri-ueno@virtualex.co.jp>
2020-05-22 18:09:01 +0900

    パスワード変更画面にバリデーションを追加

--- WebContent/js/passwordChange.js
+++ WebContent/js/passwordChange.js
@@ -2,35 +2,53 @@
  *
  */
 var changepassword = function(){
-	var inputPassword = $('#account_password').val();
-	var inputRepassword = $('#account_password_confirmation').val();
-	if(inputPassword === inputRepassword){
-		var requestQuery = {
-				password : inputPassword,
-			};
+	if ($("form").valid()) {
+		var inputPassword = $('#account_password').val();
+		var inputRepassword = $('#account_password_confirmation').val();
+		if(inputPassword === inputRepassword){
+			var requestQuery = {
+					password : inputPassword,
+				};
 
-		$.ajax({
-			type : 'POST',
-			dataType:'json',
-			url : '/BookManageSystem/api/auth/passwordChange',
-			data : requestQuery,
-			success : function(json) {
-				// サーバーとの通信に成功した時の処理
-				// 確認のために返却値を出力
-				console.log('返却値', json);
-				alert('パスワードが変更されました。');
-			},
-			error:function(XMLHttpRequest, textStatus, errorThrown){
-				// サーバーとの通信に失敗した時の処理
-				alert('パスワードが変更できませんでした。時間をおいて再度試してください。');
-				console.log(errorThrown)
-			}
-		});
-	}else{
-		alert('入力したパスワードが一致しませんでした。再度入力してください。')
+			$.ajax({
+				type : 'POST',
+				dataType:'json',
+				url : '/BookManageSystem/api/auth/passwordChange',
+				data : requestQuery,
+				success : function(json) {
+					// サーバーとの通信に成功した時の処理
+					// 確認のために返却値を出力
+					console.log('返却値', json);
+					alert('パスワードが変更されました。');
+				},
+				error:function(XMLHttpRequest, textStatus, errorThrown){
+					// サーバーとの通信に失敗した時の処理
+					alert('パスワードが変更できませんでした。時間をおいて再度試してください。');
+					console.log(errorThrown)
+				}
+			});
+		}else{
+			alert('入力したパスワードが一致しませんでした。再度入力してください。')
+		}
 	}
-
 }
+
+var initializeValidateRules = function(){
+	$("form").validate({
+		rules: {
+			"account_password": {
+				required: true,
+				minlength: 8
+			},
+			"account_password_confirmation": {
+				required: true,
+				equalTo: "#account_password",
+				minlength: 8
+			}
+		}
+	})
+}
+
 var LoginCertificate = function(){
 	$.ajax({
 		type : 'GET',
@@ -51,5 +69,6 @@ var LoginCertificate = function(){
 $(document).ready(function() {
 	  'use strict';
 	LoginCertificate();
+	initializeValidateRules()
 	$('#commit').click(changepassword);
 });
